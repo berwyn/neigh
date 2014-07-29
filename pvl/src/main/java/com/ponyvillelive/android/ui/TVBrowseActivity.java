90a2f7d48ca888ad184dc73d051b3e53f90e7cd7
berwyn <berwyn.codeweaver@gmail.com>
2014-07-28 22:31:11 -0600

    Holy shit, I don't even. Something something Android TV ADT-1

---
+++ pvl/src/main/java/com/ponyvillelive/android/ui/TVBrowseActivity.java
@@ -0,0 +1,15 @@
+package com.ponyvillelive.android.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ponyvillelive.android.R;
+
+public class TVBrowseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tvbrowse);
+    }
+}
---
+++ pvl/src/main/java/com/ponyvillelive/android/ui/TVDetailsActivity.java
@@ -0,0 +1,17 @@
+package com.ponyvillelive.android.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ponyvillelive.android.R;
+
+public class TVDetailsActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tvdetails);
+    }
+}
